d7917d7fb024b3aec0792c1240d55006022d651f
Axel Viala <aviala@student.42.fr>
2013-12-14 20:00:20 +0100

    correct prototype of read_line.c

--- read_line.c
+++ read_line.c
@@ -3,33 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   read_line.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mde-jesu <mde-jesu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aviala <aviala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2013/12/14 19:32:04 by mde-jesu          #+#    #+#             */
-/*   Updated: 2013/12/14 19:46:15 by mde-jesu         ###   ########.fr       */
+/*   Created: 2013/12/14 19:32:04 by mde               #+#    #+#             */
+/*   Updated: 2013/12/14 19:59:05 by aviala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+
 #define BUFF_SIZE 42
 
-char		*read_line(void)
+int		read_line(char **line)
 {
 	char	*buff;
-	char	*line;
-	size_t	count;
 	ssize_t len_read;
 
-	if ((buff = (char *)malloc((BUFF_SIZE + 1) * sizeof(char))) == NULL)
-		return (NULL);
-	while ((len_read = read(0, buff, BUFF_SIZE)) > 0)
+	if ((buff = (char *)malloc((BUFF_SIZE + 1) * sizeof(char))) != NULL)
 	{
-		buff[len_read] = '\0';
-		line = ft_strjoin(line, buff);
+		while ((len_read = read(0, buff, BUFF_SIZE)) > 0)
+		{
+			buff[len_read] = '\0';
+			*line = ft_strjoin_ht(*line, buff);
+		}
+		if (len_read < 0)
+			return (-1);
+		free(buff);
+		buff = NULL;
 	}
-	free(buff);
-	return (line);
+	return (0);
 }
 
 /*
