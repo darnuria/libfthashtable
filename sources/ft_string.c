d5e03a6339cd525070cb4779555d3f1e49ced462
Axel Viala <aviala@student.42.fr>
2013-12-15 09:54:50 +0100

    dodo.

--- sources/ft_string.c
+++ sources/ft_string.c
@@ -22,7 +22,7 @@ size_t	ft_ht_strlen(const char *s)
 	return ((size_t) (s_scroll - s));
 }
 
-int	ft_ht_strcmp(const char *s1, const char *s2)
+int		ft_ht_strcmp(const char *s1, const char *s2)
 {
 	while (*s1 || *s2)
 	{
@@ -34,4 +34,62 @@ int	ft_ht_strcmp(const char *s1, const char *s2)
 		s2++;
 	}
 	return (0);
+}
+
+char	*ft_ht_strncpy(char *s1, const char *s2, size_t n)
+{
+	char	s*;
+
+	s = s1;
+	while (n > 0 && *s2 != '\0')
+	{
+		*s1++ = *s2++;
+		--n;
+	}
+	while (n > 0)
+	{
+		*s1++ = '\0';
+		--n
+	}
+	return (s1);
+}
+
+char	*ft_ht_strncat(char *s1, const char *s2, size_t n)
+{
+	char	*dst;
+
+	dst = s1;
+	if (n > 0)
+	{
+		while (*dst != '\0')
+		{
+			dst++;
+		}
+		while ((*dst = *s2) && --n > 0)
+		{
+			dst++;
+			s2++;
+		}
+		dst[i] = '\0';
+	}
+	return (s1);
+}
+
+char	*ft_ht_strjoin(char const *s1, char const *s2)
+{
+	const size_t	len_s1 = ft_strlen(s1);
+	const size_t	len_s2 = ft_strlen(s2);
+	char			*new_str;
+	size_t			len_mix;
+
+	len_mix = len_s1 + len_s2
+	if ((new_str = (char *) malloc(sizeof(char) * (len_mix + 1))) != NULL)
+	{
+		if ((len_mix) != 0)
+		{
+			ft_strncpy(new_str, s1, len_s1);
+			ft_strncat(new_str, s2, len_mix);
+		}
+	}
+	return (new_str);
 }
--- sources/ft_utils.c
+++ sources/ft_utils.c
@@ -16,85 +16,29 @@
 #include <string.h>
 #include "read_line.h"
 
-static char	*ft_strncpy(char *s1, const char *s2, size_t n)
-{
-	char	s*;
-
-	s = s1;
-	while (n > 0 && *s2 != '\0')
-	{
-		*s1++ = *s2++;
-		--n;
-	}
-	while (n > 0)
-	{
-		*s1++ = '\0';
-		--n
-	}
-	return (s1);
-}
-
-static char	*ft_ht_strncat(char *s1, const char *s2, size_t n)
-{
-	char		*dst;
-
-	dst = s1;
-	if (n > 0)
-	{
-		while (*dst != '\0')
-		{
-			dst++;
-		}
-		while ((*dst = *s2) && --n > 0)
-		{
-			dst++;
-			s2++;
-		}
-		dst[i] = '\0';
-	}
-	return (s1);
-}
-
-static char	*ft_ht_strjoin(char const *s1, char const *s2)
-{
-	const size_t	len_s1 = ft_strlen(s1);
-	const size_t	len_s2 = ft_strlen(s2);
-	char			*new_str;
-	size_t			len_mix;
-
-	len_mix = len_s1 + len_s2
-	if ((new_str = (char *) malloc(sizeof(char) * (len_mix + 1))) != NULL)
-	{
-		if ((len_mix) != 0)
-		{
-			ft_strncpy(new_str, s1, len_s1);
-			ft_strncat(new_str, s2, len_mix);
-		}
-	}
-	return (new_str);
-}
-
-char	*read_line(void)
-{
-	char	*buff;
-	char	*line;
-	ssize_t len_read;
-
-	if ((buff = (char *) malloc((BUFF_SIZE + 1) * sizeof(char))) != NULL)
-	{
-		while ((len_read = read(0, buff, BUFF_SIZE)) > 0)
-		{
-			buff[len_read] = '\0';
-			line = ft_ht_strjoin(line, buff);
-		}
-		if (len_read < 0)
-			return (-1);
-		free(buff);
-		buff = NULL;
-	}
-	return (0);
-}
-
 /*
-** ssize_t read(int fd, void *buf, size_t count);
+** int
+**
+**
 */
+
+/* Pourri recode la. */
+int		read_line(const int fd, char **line)
+{
+	static char	*rest = NULL;
+	ssize_t		ret;
+
+	if (fd < 0)
+		return (-1);
+	if (rest == NULL)
+	{
+		if ((rest = (char*) malloc(sizeof(char) * 1)) == NULL)
+			return (-1);
+	}
+	if ((ret = ft_read_fd(fd, &ret)) == -1)
+		return (-1);
+	else if (ret == 0 && *rest == '\0')
+		return (0);
+	else
+		return (ft_line(line, rest, ret);
+}
